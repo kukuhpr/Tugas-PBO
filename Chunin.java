1e7ee705f7fef2e1c02528ded0329211c0794176
kukuhpr <primaditokukuh@gmail.com>
2018-10-14 19:05:17 +0700

    first commit

---
+++ Chunin.java
@@ -0,0 +1,22 @@
+public class Chunin{
+  private String Reg, Nama, Peringkat;
+
+  public void setReg (String Reg){
+    this.Reg = Reg;
+  }
+  public String getReg(){
+    return Reg;
+  }
+  public void setNama(String Nama){
+    this.Nama = Nama;
+  }
+  public String getNama(){
+    return Nama;
+  }
+  public void setPeringkat(String Peringkat){
+    this.Peringkat = Peringkat;
+  }
+  public String getPeringkat(){
+    return Peringkat;
+  }
+}
